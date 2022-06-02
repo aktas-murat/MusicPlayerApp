d4e5c0ebc9fdbf0e193a2b69873375fdbfcf7c4f
Murat Aktaş <murat.aktaas@hotmail.com>
2022-06-02 23:06:28 +0300

    [ınit] Added Project Files.

---
+++ MusicPlayerApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  MusicPlayerApp
+//
+//  Created by Murat Aktaş on 6/2/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		
+		return true
+		
+	}
+	
+}
